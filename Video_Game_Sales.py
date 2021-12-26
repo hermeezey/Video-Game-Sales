0f4fe39fe498e90ca7224b774f0272c372885d77
hermeezey <fledglingfeenix@gmail.com>
2021-12-26 11:16:19 -0800

    Initial Commit

---
+++ Video_Game_Sales.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 18:07:02 2021
+
+@author: Herman
+"""
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+vg_data = pd.read_csv('Video_Games.csv')
+
+vg_data_UserScore = vg_data[vg_data['User_Score']!='tbd']
+vg_data_UserScore['User_Score'] = vg_data_UserScore['User_Score'].astype(float)
+
+fig1, ax1 = plt.subplots()
+
+
+
+sns.histplot(data = vg_data['Critic_Score'].dropna(), ax = ax1, stat = 'probability', binwidth=2,
+             binrange = (0,100))
+
+fig2, ax2 = plt.subplots()
+
+sns.histplot(data = vg_data_UserScore['User_Score'].dropna(), ax = ax2, 
+             stat = 'probability', binwidth=1, binrange = (0,10))
+
+
+
+
