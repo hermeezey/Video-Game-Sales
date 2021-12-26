2e916df2646fdfae2adba7b61f2f78a3a7ba5f43
hermeezey <fledglingfeenix@gmail.com>
2021-12-26 12:25:59 -0800

    Created csv file with summary statistics

--- Video_Game_Sales.py
+++ Video_Game_Sales.py
@@ -11,11 +11,12 @@ import seaborn as sns
 
 vg_data = pd.read_csv('Video_Games.csv')
 
+vg_data.describe().to_csv('Video_Games_Summary.csv')
+
 vg_data_UserScore = vg_data[vg_data['User_Score']!='tbd']
 vg_data_UserScore['User_Score'] = vg_data_UserScore['User_Score'].astype(float)
 
 fig1, ax1 = plt.subplots()
-
 
 
 sns.histplot(data = vg_data['Critic_Score'].dropna(), ax = ax1, stat = 'probability', binwidth=2,
@@ -24,7 +25,7 @@ sns.histplot(data = vg_data['Critic_Score'].dropna(), ax = ax1, stat = 'probabil
 fig2, ax2 = plt.subplots()
 
 sns.histplot(data = vg_data_UserScore['User_Score'].dropna(), ax = ax2, 
-             stat = 'probability', binwidth=1, binrange = (0,10))
+             stat = 'probability', binwidth=0.5, binrange = (0,10))
 
 
 
